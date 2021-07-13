c367e53953a97afcb38c40d04f5a67663a96e161
atsushi oshima <aboutagial@yahoo.co.jp>
2021-07-13 18:50:41 +0900

    rb2

--- ruby_exam/janken.rb
+++ ruby_exam/janken.rb
@@ -84,12 +84,12 @@ class GameStart
 
     # 変数「next_game」に「true」を代入しましょう。
     # 「next_game」が「false」だと繰り返し処理を終了し、「true」だと繰り返し処理を継続します。
-    next_game = true
-    while next_game 
+    #next_game = true
+    #while next_game 
       # 変数「next_game」にじゃんけんを実行して返ってきた値(戻り値)を代入します。
       #「janken.pon(player.hand, enemy.hand)」でじゃんけんを実行しています。
-       next_game = janken.pon(player.hand, enemy.hand)
-    end
+       #next_game = janken.pon(player.hand, enemy.hand)
+    #end
   end
 end
 
