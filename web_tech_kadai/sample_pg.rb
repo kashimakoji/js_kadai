a5034efd5e89534c3c4eed9bfa669bb77a1a7b58
atsushi oshima <aboutagial@yahoo.co.jp>
2021-07-21 13:58:54 +0900

    web2

--- web_tech_kadai/sample_pg.rb
+++ web_tech_kadai/sample_pg.rb
@@ -2,11 +2,11 @@ require 'pg'
 connection = PG::connect(dbname: "goya") #PG::connect(dbname:goya)により、rubyからgoyaDBに接続し
 connection.internal_encoding = "UTF-8"   #接続したという情報をconnectionという変数に入れる
 begin
-    result = connection.exec("select wight, give_for from crops;")
+    result = connection.exec("select weight, give_for from crops;")
     #connection変数を使いPostgreSQLを操作する。.execで、goyaDBに"select weight, give_for from crops;"の
     #SQLの命令文を直接実行し、その結果をresultに入れる。
     result.each do |record|     #取り出した各行を処理する。各行をとり出しターミナル上に出力する
-        puts "ゴーヤの大きさ：#{record["wight"]} 売った相手：#{record["give_for"]}"
+        puts "ゴーヤの大きさ：#{record["weight"]} 売った相手：#{record["give_for"]}"
     end
 
 
